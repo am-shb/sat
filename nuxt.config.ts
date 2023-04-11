84e1816327b91bb0230ffb51be29006e681f361c
Amir Shahbandegan <am.shahbandegan@gmail.com>
2023-04-10 22:24:28 -0400

    Update nuxt config to detect repo name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,5 +48,17 @@ export default defineNuxtConfig({
   },
   hooks: {
     "nitro:build:public-assets": copyAssets,
+    "nitro:config": (nitroConfig) => {
+      let repo_name = process.env.GH_PAGES_REPO_NAME;
+      if (repo_name) {
+        console.log(
+          "Building for GitHub Pages -- GH_PAGES_REPO_NAME=",
+          repo_name
+        );
+        const baseUrl = "/" + repo_name + "/";
+        nitroConfig.baseURL = baseUrl;
+        nitroConfig.runtimeConfig.app.baseURL = baseUrl;
+      }
+    },
   },
 });
